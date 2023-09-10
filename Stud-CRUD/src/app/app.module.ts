2ce9011ef34734838db22a3dc67f70c0aab000d4
Boudjema DJEBARRI <117278502+BoudjemaDJEBARRI@users.noreply.github.com>
2023-09-10 11:32:35 +0200

    V1

---
+++ Stud-CRUD/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { VerifyEmailComponent } from './pages/verify-email/verify-email.component';
+import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    VerifyEmailComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Stud-CRUD/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'djeb-scool',
+    appId: '1:208575162369:web:e694624851329ea226f800',
+    storageBucket: 'djeb-scool.appspot.com',
+    apiKey: 'AIzaSyB28CYrmGl_R7NLFnE8u2UJNtrytfV_gLM',
+    authDomain: 'djeb-scool.firebaseapp.com',
+    messagingSenderId: '208575162369',
+    measurementId: 'G-C0200VWRG0',
+  },
+};
